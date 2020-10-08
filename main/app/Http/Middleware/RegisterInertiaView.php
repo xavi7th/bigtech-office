31959d72e9d33b641e948beee90bc50f56d0a7cf
Leinad <xavi7th@gmail.com>
2020-10-08 06:41:37 +0100

    Chore: Refactor register Inertia view to  accomodate any user type

--- main/app/Http/Middleware/RegisterInertiaView.php
+++ main/app/Http/Middleware/RegisterInertiaView.php
@@ -18,16 +18,13 @@ class RegisterInertiaView
   public function handle($request, Closure $next)
   {
     // dump(\Route::currentRouteName());
-    // dump(Str::contains(\Route::currentRouteName(), 'login'));
     // dd(routeHasRootNamespace('app.'));
-    if (routeHasRootNamespace('app.') && !Str::contains(\Route::currentRouteName(), 'login')) {
-      Inertia::setRootView('publicpages::app');
-    } elseif (routeHasRootNamespace('appuser.') || Str::contains(\Route::currentRouteName(), 'login')) {
+    if ($request->user()) {
+      Inertia::setRootView(strtolower($request->user()->getType()) . '::app');
+    } elseif (Str::contains(\Route::currentRouteName(), 'login')) {
       Inertia::setRootView('appuser::app');
-    } elseif (routeHasRootNamespace('admin.')) {
-      Inertia::setRootView('admin::app');
-    } elseif (routeHasRootNamespace('superadmin.')) {
-      Inertia::setRootView('superadmin::app');
+    } else {
+      Inertia::setRootView('publicpages::app');
     }
     return $next($request);
   }
