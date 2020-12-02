d28435b265874148dde9209a8d037716ee5f8b94
Leinad <xavi7th@gmail.com>
2020-12-02 08:04:03 +0100

    Increase the duration of batches stock keeper can view

--- main/app/Modules/SuperAdmin/Models/Product.php
+++ main/app/Modules/SuperAdmin/Models/Product.php
@@ -504,7 +504,7 @@ class Product extends BaseModel
   public function showCreateProductForm()
   {
     return Inertia::render('SuperAdmin,Products/CreateProduct', [
-      'batches' => fn () => Cache::remember('batches', (15 * 60 * 60), fn () => (new ProductBatchTransformer)->collectionTransformer(ProductBatch::whereDate('created_at', '>', now()->subDays(2))->foreign()->latest()->take(2)->get(), 'basic')),
+      'batches' => fn () => Cache::remember('batches', (15 * 60 * 60), fn () => (new ProductBatchTransformer)->collectionTransformer(ProductBatch::whereDate('created_at', '>', now()->subDays(4))->foreign()->latest()->take(2)->get(), 'basic')),
       'categories' => fn () => Cache::rememberForever('categories', fn () => (new ProductCategoryTransformer)->collectionTransformer(ProductCategory::all(), 'basic')),
       'models' => fn () => Cache::rememberForever('models', fn () => (new ProductModelTransformer)->collectionTransformer(ProductModel::all(), 'basic')),
       'brands' => fn () => Cache::rememberForever('brands', fn () => (new ProductBrandTransformer)->collectionTransformer(ProductBrand::all(), 'basic')),
