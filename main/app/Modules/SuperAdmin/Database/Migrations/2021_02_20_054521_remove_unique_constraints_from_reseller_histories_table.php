01c1a8e7e3b5811a17fb180b50062651c4e40291
Leinad <xavi7th@gmail.com>
2021-02-20 19:10:36 +0100

    Fix: Migration fails SQL error cannt drop foreign key in reseler histories error

--- main/app/Modules/SuperAdmin/Database/Migrations/2021_02_20_054521_remove_unique_constraints_from_reseller_histories_table.php
+++ main/app/Modules/SuperAdmin/Database/Migrations/2021_02_20_054521_remove_unique_constraints_from_reseller_histories_table.php
@@ -14,6 +14,7 @@ class RemoveUniqueConstraintsFromResellerHistoriesTable extends Migration
   public function up()
   {
     Schema::table('reseller_histories', function (Blueprint $table) {
+      $table->index('reseller_id', 'reseller_id_foreign');
       $table->dropUnique('reseller_histories_unique');
     });
   }
