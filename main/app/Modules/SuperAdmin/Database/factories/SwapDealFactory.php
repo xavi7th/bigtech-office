c3342f8a8854007e1ac760657faf53a7378f2e33
Leinad <xavi7th@gmail.com>
2021-02-28 17:01:18 +0100

    Create Swal=pdeal factory

---
+++ main/app/Modules/SuperAdmin/Database/factories/SwapDealFactory.php
@@ -0,0 +1,18 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Modules\SuperAdmin\Models\SwapDeal;
+use Faker\Generator as Faker;
+
+$factory->define(SwapDeal::class, function (Faker $faker) {
+  return [
+    'description' => $faker->sentence,
+    'owner_details' => $faker->name,
+    'id_url' => $faker->imageUrl(),
+    'receipt_url' => $faker->imageUrl(),
+    'imei' => $faker->bankAccountNumber,
+    'swap_value' => $faker->randomNumber(5),
+    'selling_price' => $faker->randomNumber(6),
+  ];
+});
